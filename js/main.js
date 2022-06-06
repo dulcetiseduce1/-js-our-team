fdbb6a4717da689e92dd9df7f219cb5279cbb987
dulcetiseduce1 <mariusdulce@tuta.io>
2022-06-06 16:49:07 +0200

    push array

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//html classe
+const teamContainer = document.querySelector(".team-container");
+
+//array membri
+const membriTeam = [
+    {
+        nome:'Wayne Barrett',
+        ruolo:'Founder & CEO',
+        foto:'img/wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome:'Angela Caroll',
+        ruolo:'Chief Editor',
+        foto:'img/angerla-caroll-chief-editor.jpg',
+    },
+    {
+        nome:'Walter Gordon',
+        ruolo:'Office Manager',
+        foto:'img/walter-gordon-office-manager.jpg',
+    },
+    {
+        nome:'Angela Lopez',
+        ruolo:'Social Media Manager',
+        foto:'img/angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome:'Scott Estrada',
+        ruolo:'Developer',
+        foto:'img/scott-estrada-developer.jpg',
+    },
+    {
+        nome:'Barabara Ramos',
+        ruolo:'Graphic Designer',
+        foto:'img/barbara-ramos-graphic-designer.jpg',
+    },
+]
+
+//ciclo per stampare
+for(let i=0; i < membriTeam.length;i++){
+    const membro
+}
